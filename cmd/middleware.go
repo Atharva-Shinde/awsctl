8015302bc635e11fbd4f49eee691377e6be47660
Atharva Shinde <atharvashinde179@gmail.com>
2023-02-05 01:59:01 +0530

    redefined and refactor: api logic

--- cmd/middleware.go
+++ cmd/middleware.go
@@ -1,41 +1,50 @@
 package cmd
 
 import (
-	// "github.com/aws/aws-sdk-go-v2/aws"
 	"context"
 	"fmt"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/eks"
+	"github.com/aws/aws-sdk-go-v2/service/eks/types"
 )
 
 type Configuration struct {
-	config      config.Config
-	credentials aws.Credentials
+	cfg aws.Config
 }
 
-func (config *Configuration) worker() {
+func worker() {
+	cf := Configuration{
+		cfg: aws.Config{
+			Region:      "india",
+			Credentials: aws.NewConfig().Credentials,
+		},
+	}
 
-	// c := Configuration{
-	// 	config: aws.Config{
-	// 		Region: "india",
-	// 	},
-	// 	credentials: aws.Credentials{
-	// 		AccessKeyID:     "kjsjlksjl",
-	// 		SecretAccessKey: "lsjjlsd",
-	// 	},
-	// }
+	x := eks.NewFromConfig(cf.cfg)
+	clusterinfo := eks.CreateClusterInput{
+		Name: aws.String("dlkjdl"),
+		ResourcesVpcConfig: &types.VpcConfigRequest{
+			EndpointPrivateAccess: aws.Bool(false),
+			EndpointPublicAccess:  aws.Bool(false),
+			PublicAccessCidrs:     aws.ToStringSlice([]*string{aws.String("dfjk")}),
+			SecurityGroupIds:      aws.ToStringSlice([]*string{aws.String("oirio")}),
+			SubnetIds:             aws.ToStringSlice([]*string{aws.String("aklkld")}),
+		},
+		RoleArn:                 aws.String("jldkd"),
+		ClientRequestToken:      aws.String("lkjddl"),
+		EncryptionConfig:        []types.EncryptionConfig{},
+		KubernetesNetworkConfig: &types.KubernetesNetworkConfigRequest{IpFamily: "lfdjk", ServiceIpv4Cidr: aws.String("jlkd")},
+		Logging:                 &types.Logging{ClusterLogging: []types.LogSetup{}},
+		Version:                 aws.String("132"),
+	}
 
-	x := eks.New(eks.Options{Credentials: aws.NewConfig().Credentials})
 	ctx := context.Background()
-	params := &eks.CreateClusterInput{
-		Name: aws.String("mycluster"),
-	}
-	output, err := x.CreateCluster(ctx, params)
+	b, err := x.CreateCluster(ctx, &clusterinfo)
 	if err != nil {
-		fmt.Print("error")
+		fmt.Print(err)
 	}
-	fmt.Print(output)
+
+	fmt.Print(b)
 
 }
